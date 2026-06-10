e79682b84015dec76aa823680789c4001abbd8d0
catalina-accr <catalina.accr@gmail.com>
2026-06-10 14:04:02 -0500

    chore: initialize React project with Vite

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Controla tus finanzas como un profesional</h1>
+
+      <p>
+        Gestiona gastos, ahorros e inversiones
+        desde una sola plataforma.
+      </p>
+
+      <button>Comenzar Gratis</button>
+    </section>
+  )
+}
+
+export default Hero
